6c91b67a49e54d3f2a7dcb6abe76f96796af7761
Oskar Zhang <oskarzhang@v1021-wn-87-227.campus-dynamic.uwaterloo.ca>
2015-09-19 01:13:45 -0400

    initial commit

---
+++ SilverApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SilverApp
+//
+//  Created by Oskar Zhang on 9/19/15.
+//  Copyright © 2015 Silver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
